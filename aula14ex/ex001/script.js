c2dbfadef07898cc6482d237a9306dade5a441d6
FranciscoAMR2001 <155982107+FranciscoAMR2001@users.noreply.github.com>
2024-04-01 09:36:48 -0300

    ex 02 aula 14

--- aula14ex/ex001/script.js
+++ aula14ex/ex001/script.js
@@ -12,7 +12,7 @@ function contar() {
        let i = Number(ini.value) 
        let f = Number(fim.value)
        let p = Number(passo.value)
-       if (p <=0) {
+       if (p <= 0) {
         window.alert('Passo inválido! Considerando PASSO 1')
         p = 1
        }
